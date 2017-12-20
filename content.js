2436f9d78d3c2ec7f8c0c918e028d83f1162d190
BruceKey3 <mrbrucekey3@gmail.com>
2017-12-20 12:45:46 +0000

    Add files via upload

---
+++ content.js
@@ -0,0 +1,17 @@
+// content.js
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+
+      // This line is new!
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+    }
+    else if (request.message === "onHeadersReceived") {
+      console.log("Hello");
+    }
+  }
+);
