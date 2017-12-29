d0a54d44d36ca6b8ef70032ca77b74f0df53bc7b
Bruce Key <mrbrucekey3@gmail.com>
2017-12-29 11:58:54 +0000

    debugger work

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,4 +1,6 @@
+/*
 chrome.webRequest.onHeadersReceived.addListener(function(details){
+  console.log("Headers:")
   console.log(details.responseHeaders);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
@@ -16,7 +18,45 @@ chrome.webRequest.onBeforeRequest.addListener(
   },
   {urls: ["<all_urls>"]},
   ["blocking", "requestBody"]);
+*/
+var version = "1.0";
 
+chrome.browserAction.onClicked.addListener(function(tab) {
+  //console.log(tab);
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    let id = activeTab.id;
+    console.log(id);
+    chrome.tabs.sendMessage(id, {
+        "message": "browserAction_clicked",
+        "data": id
+    });
+  });
+});
 
+function onAttach(tabId) {
+  if (chrome.runtime.lastError) {
+    alert(chrome.runtime.lastError.message);
+    return;
+  }
+
+  chrome.windows.create(
+      {url: "html/headers.html?" + tabId,
+       type: "popup",
+       width: 800, height: 600});
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "openWindow" ) {
+      let id = request.data;
+      console.log(request);
+
+      chrome.debugger.attach({tabId: id}, version,
+          onAttach.bind(null, id)
+      );
+    }
+
+});
 
 console.log("background");
--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,15 +1,46 @@
 // content.js
 
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "onHeadersReceived" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-      //console.log(request.message);
+    if( request.message === "browserAction_clicked" ) {
+      //var firstHref = $("a[href^='http']").eq(0).attr("href");
+      console.log(request);
+      let id = request.data;
 
+      chrome.runtime.sendMessage({
+        "message": "openWindow",
+        "data": id
+      });
+
+      /*
       if (request.data[0].value >= 300 && request.data[0].value < 400) {
         console.log("Redirect");
         console.log(request.data[0]);
       }
+      */
     }
-  }
-);
+});
+
+/*
+console.log("Scripts:");
+let scriptList = document.scripts;
+
+for (let script of scriptList) {
+  console.log(script);
+}
+*/
+
+/*
+// How to send Post request + do something with result
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status != 420) {
+       // Typical action to be performed when the document is ready:
+       console.log("Response:");
+       console.log(xhttp.responseText);
+    }
+};
+xhttp.open("POST", "https://google.com");
+xhttp.send();
+*/
